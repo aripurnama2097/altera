67b2c223645dc3ab6cfa124dc2ec5fb4fd22809e
purnama <aripurnama2097@gmail.com>
2023-03-29 11:05:45 +0700

    ALTERA - revisi name login

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -5,23 +5,16 @@
         </div>
         <nav class="nav navbar-nav">
         <ul class=" navbar-right">
+          @auth
           <li class="nav-item dropdown open" style="padding-left: 15px;">
             <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
-             {{-- {{auth()->user()->name}} --}}
-      
+             {{auth()->user()->name}}  
             </a>
             <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
-              {{-- <a class="dropdown-item"  href="javascript:;"> Profile</a>
-                <a class="dropdown-item"  href="javascript:;">
-                  <span class="badge bg-red pull-right">50%</span>
-                  <span>Settings</span>
-                </a>
-            <a class="dropdown-item"  href="javascript:;">Help</a> --}}
               <a class="dropdown-item"  href="{{url('/login')}}"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
             </div>
           </li>
-
-       
+          @endauth
         </ul>
       </nav>
     </div>
