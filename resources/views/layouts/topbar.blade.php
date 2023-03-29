01358e72ce1d0fa7bb95a7cf82123655b9e0a8e4
purnama <aripurnama2097@gmail.com>
2023-03-29 09:46:21 +0700

    ALTERA - rev name topbar

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -7,7 +7,8 @@
         <ul class=" navbar-right">
           <li class="nav-item dropdown open" style="padding-left: 15px;">
             <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
-             {{auth()->user()->name}}
+             {{-- {{auth()->user()->name}} --}}
+      
             </a>
             <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
               {{-- <a class="dropdown-item"  href="javascript:;"> Profile</a>
